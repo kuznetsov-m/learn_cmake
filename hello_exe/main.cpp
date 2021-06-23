890c67ac2811811844825f7d531afa24a3f5ddc3
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2021-06-24 00:08:30 +0300

    Add Subdirectories and Target Interface

---
+++ hello_exe/main.cpp
@@ -0,0 +1,5 @@
+#include <say_hello/hello.h>
+
+int main() {
+    hello::say_hello();
+}
---
+++ say_hello/src/say_hello/hello.cpp
@@ -0,0 +1,7 @@
+#include "hello.h"
+
+#include <iostream>
+
+void hello::say_hello() {
+    std::cout << "Hi there!" << std::endl;
+}
